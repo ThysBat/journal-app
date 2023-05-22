bd083105684e152fe3fd31ffccfe3d16bd0cd84f
David Illies <dav.illies@web.de>
2023-05-22 09:20:25 +0200

    change typo

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -5,10 +5,10 @@ import Tab from "../Tab";
 export default function Tabs() {
   return (
     <section className="tabs">
-      <Tab counter={3} highlighted>
+      <Tab numberOfEntries={3} highlighted>
         All Entries
       </Tab>
-      <Tab counter={1}>Favorites</Tab>
+      <Tab numberOfEntries={1}>Favorites</Tab>
     </section>
   );
 }
